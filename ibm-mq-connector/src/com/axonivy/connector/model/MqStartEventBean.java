fec97f7913fb8637080c65daf8a00718e395801c
Son Quach <son.quach@axonactive.com>
2026-06-22 17:03:37 +0700

    feat: add MqStartEventBean

---
+++ ibm-mq-connector/src/com/axonivy/connector/model/MqStartEventBean.java
@@ -0,0 +1,12 @@
+package com.axonivy.connector.model;
+
+import ch.ivyteam.ivy.process.eventstart.AbstractProcessStartEventBean;
+
+public class MqStartEventBean extends AbstractProcessStartEventBean {
+
+	protected MqStartEventBean(String name, String description) {
+		super(name, description);
+	}
+
+	
+}
