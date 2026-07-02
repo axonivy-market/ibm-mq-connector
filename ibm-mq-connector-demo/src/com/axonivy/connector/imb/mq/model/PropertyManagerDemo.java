f6fa1d1275501d4bc50d25c3a903ad5ab50540e3
Son Quach <son.quach@axonactive.com>
2026-07-02 17:58:32 +0700

    refactor: empty variables default and handle Boolean variables

--- ibm-mq-connector-demo/src/com/axonivy/connector/imb/mq/model/PropertyManagerDemo.java
+++ ibm-mq-connector-demo/src/com/axonivy/connector/imb/mq/model/PropertyManagerDemo.java
@@ -1,5 +1,9 @@
 package com.axonivy.connector.imb.mq.model;
 
+
+
+import org.apache.commons.lang3.StringUtils;
+
 import ch.ivyteam.ivy.environment.Ivy;
 
 public class PropertyManagerDemo {
@@ -7,7 +11,8 @@ public class PropertyManagerDemo {
 	private static final String SKIP_LISTENER_PROPERTY = IBM_ROOT_PROPERTY + "skipListener";
 
 	public static boolean getSkipListener() {
-		return Boolean.parseBoolean(Ivy.var().get(SKIP_LISTENER_PROPERTY));
+		String value = Ivy.var().get(SKIP_LISTENER_PROPERTY);
+		return  StringUtils.isNotBlank(value) && Boolean.parseBoolean(value);
 	}
 
 }
