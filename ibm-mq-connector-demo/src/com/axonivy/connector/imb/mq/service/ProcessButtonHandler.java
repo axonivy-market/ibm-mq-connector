98642df08a5474e88f00fd13e30ffc555963e4aa
Son Quach <qtson@axonactive.com>
2026-07-03 09:22:26 +0700

    feat: handle copilot feedback

--- ibm-mq-connector-demo/src/com/axonivy/connector/imb/mq/service/ProcessButtonHandler.java
+++ ibm-mq-connector-demo/src/com/axonivy/connector/imb/mq/service/ProcessButtonHandler.java
@@ -4,17 +4,19 @@ import com.axonivy.connector.service.MQueueListener;
 
 public class ProcessButtonHandler {
 	private static ProcessButtonHandler INSTANCE = new ProcessButtonHandler();
-	
+
 	public static ProcessButtonHandler getInstance() {
 		return INSTANCE;
 	}
 
-	public void process(String messageType, String queueName) {	
+	public void process(String messageType, String queueName) {
 		MQueueListener mqListener = new MQueueListener(queueName, new ProcessMessageFilterHandler(messageType));
-		mqListener.start();
-		mqListener.receiveNoWait();		
-		mqListener.stop();
+		try {
+ 			mqListener.start();
+ 			mqListener.receiveNoWait();
+ 		} finally {
+ 			mqListener.stop();
+ 		}
 	}
-	
-	
+
 }
